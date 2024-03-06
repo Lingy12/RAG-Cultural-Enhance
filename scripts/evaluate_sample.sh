2c3cc40e55bf90965ed347474f3c96225a2242a4
Lin Geyu <geyu.lin@u.nus.edu>
2024-03-06 17:11:21 +0800

    add evaluate sample

--- scripts/evaluate_sample.sh
+++ scripts/evaluate_sample.sh
@@ -1,3 +1,5 @@
 python src/evaluator.py /home/shared_LLMs/gemma-2b-it prompt/prompt_eval.txt ./vector_store/wiki_filter_country_bge-large-en-v1.5 0.5 6 "sg_eval,us_eval,ph_eval" embed BAAI/bge-large-en-v1.5 
 
+python src/evaluator.py /home/shared_LLMs/gemma-2b-it prompt/prompt_eval.txt ./vector_store/wiki_sg_exclusive_1_1_None_1_1.0_True.pkl/ 0.2 8 "sg_eval," tfidf
+
 
