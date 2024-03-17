ce919beb6b6c138d1e05a058d82dbe027913a015
Lin Geyu <geyu.lin@u.nus.edu>
2024-03-17 18:39:13 +0800

    support generate result table

--- generate_result_table.py
+++ generate_result_table.py
@@ -14,9 +14,23 @@ def consolidate_exp_folder(folder, output_path=None):
                     "rag_error": [], "not_trigger_rag": [], 
                     "total_accuracy": [], "vector_store_name":[], "retrieval_threshold":[], 
                     "retrieval_number":[], "need_rerank":[], "doc_final": [], "model_type": [], 
-                    "meta_data": [],
+                    "meta_data": [], 
                     "vector_store type": []}
-    
+    embedding_table = {"run_name": [], "revise_count": [], 
+                    "rag_error": [], "not_trigger_rag": [], 
+                    "total_accuracy": [], "vector_store_name":[], "retrieval_threshold":[], 
+                    "retrieval_number":[], "need_rerank":[], "doc_final": [], "model_type": [], 
+                    "meta_data": [], 
+                    "vector_store type": []}
+    tf_idf_table = {"run_name": [], "revise_count": [], 
+                    "rag_error": [], "not_trigger_rag": [], 
+                    "total_accuracy": [], "vector_store_name":[], "n_min":[], "n_max": [] ,
+                     "max_features":[], "min_df": [], "max_df": [], "use_idf": [],
+                    "retrieval_threshold":[], 
+                    "retrieval_number":[], "need_rerank":[], "doc_final": [], "model_type": [], 
+                    "vector_store type": []
+        
+    }
     for pred in prediction_files:
         compare_res = compare_files(baseline_file, pred)
         
@@ -60,12 +74,27 @@ def consolidate_exp_folder(folder, output_path=None):
         
         for k in list(output_table.keys())[5:12]:
             output_table[k].append(eval(k))
-              
+            
+        if output_table['vector_store type'][-1] == 'tf-idf':
+            for k in tf_idf_table.keys():
+                if k in output_table:
+                    tf_idf_table[k].append(output_table[k][-1])
+                else:
+                    tf_idf_table[k].append(json.loads(meta_data)[k])
+        else:
+            for k in embedding_table.keys():
+                embedding_table[k].append(output_table[k][-1])
     data = pd.DataFrame.from_dict(output_table)
+    embedding = pd.DataFrame.from_dict(embedding_table)
+    tf_idf = pd.DataFrame.from_dict(tf_idf_table)
     print(data)
-    print(data['run_name'])
+    print(embedding)
+    print(tf_idf)
     if output_path:
-        data.to_excel(output_path)
-        
+        writer = pd.ExcelWriter(output_path, engine='openpyxl')
+        data.to_excel(writer, sheet_name='overall_results')
+        embedding.to_excel(writer, sheet_name='embedding results')
+        tf_idf.to_excel(writer, sheet_name='tf-idf results')
+        writer.close()
 if __name__ == '__main__':
     fire.Fire(consolidate_exp_folder)
